4c06f991f36598efe0da549295a471635a16b5c9
alexislom <alexislomako@gmail.com>
2020-06-07 22:54:35 +0300

    Add first model view

---
+++ CatharsisSampleForms/ViewModels/FirstModalViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Reactive;
+using Catharsis.Navigation.Abstractions;
+using ReactiveUI;
+
+namespace CatharsisSampleForms.ViewModels
+{
+    public class FirstModalViewModel : BaseViewModel, IDestructible
+    {
+        public ReactiveCommand<Unit, Unit> OpenModal { get; set; }
+
+        public ReactiveCommand<Unit, Unit> PopModal { get; set; }
+
+        public override string Id => nameof(FirstModalViewModel);
+
+        public FirstModalViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            //OpenModal = ReactiveCommand
+            //            .CreateFromObservable(() =>
+            //                ViewStackService.PushModal(new SecondModalViewModel(viewStackService)),
+            //                outputScheduler: RxApp.MainThreadScheduler);
+
+            //PopModal = ReactiveCommand
+            //            .CreateFromObservable(() =>
+            //                ViewStackService.PopModal(),
+            //                outputScheduler: RxApp.MainThreadScheduler);
+
+            //OpenModal.Subscribe(x => Debug.WriteLine("PagePushed"));
+            //PopModal.Subscribe(x => Debug.WriteLine("PagePopped"));
+            //PopModal.ThrownExceptions.Subscribe(error => Interactions.ErrorMessage.Handle(error).Subscribe());
+        }
+
+
+        public void Destroy()
+        {
+            System.Diagnostics.Debug.WriteLine($"Destroy: {nameof(FirstModalViewModel)}");
+        }
+    }
+}
---
+++ CatharsisSampleForms/Views/FirstModalView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using CatharsisSampleForms.ViewModels;
+using ReactiveUI.XamForms;
+
+namespace CatharsisSampleForms.Views
+{
+    public partial class FirstModalView : ReactiveContentPage<FirstModalViewModel>
+    {
+        public FirstModalView()
+        {
+            InitializeComponent();
+        }
+    }
+}
